7326bd4ac33f0b6af9e02eaa41433626f5134957
AshenWolf_v2.5.7 <146616796+DustDeuce@users.noreply.github.com>
2026-06-08 22:24:45 +0400

    Добавления основных файлов
    Добавил часть основных файлов в корневую папку директории

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/img.ico">
+    <link rel="stylesheet" href="/CSS/style.css">
+    <title>game test</title>
+</head>
+<body>
+    <!--Основная часть сайта-->
+    <div>
+        <button type="submit" onclick="document.location = '/register.php'">Начать свой путь</button>
+    </div>
+</body>
+</html>
